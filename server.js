048254658e4d2a601cd4f01d3e3943bb435278d8
Annie <anniebui0110@gmil.com>
2023-08-29 21:32:36 -0500

    moved notes.html

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) =>
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, './notes.html'))
 );
 app.get('/api/notes', (req, res) => {
     // res.sendFile(path.join(__dirname, './db/db.json'))
